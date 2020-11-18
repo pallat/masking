af904d100b22317bb801d3c5b87d63823f35e5f2
Pallat Anchaleechamaikorn <yod.pallat@gmail.com>
2020-11-18 10:36:59 +0700

    refactor move replacer out to global

--- masking.go
+++ masking.go
@@ -5,22 +5,24 @@ import (
 	"strings"
 )
 
+var creditCardReplacer = strings.NewReplacer("0", "*", "1", "*", "2", "*", "3", "*", "4", "*", "5", "*", "6", "*", "7", "*", "8", "*", "9", "*")
+
 type CreditCard string
 
 func (c CreditCard) MarshalJSON() ([]byte, error) {
-	r := strings.NewReplacer("0", "*", "1", "*", "2", "*", "3", "*", "4", "*", "5", "*", "6", "*", "7", "*", "8", "*", "9", "*")
 	x := len(c) - 4
 
-	return json.Marshal(r.Replace(string(c)[:x]) + string(c)[x:])
+	return json.Marshal(creditCardReplacer.Replace(string(c)[:x]) + string(c)[x:])
 }
+
+var nationalIDReplacer = strings.NewReplacer("0", "*", "1", "*", "2", "*", "3", "*", "4", "*", "5", "*", "6", "*", "7", "*", "8", "*", "9", "*")
 
 type NationalID string
 
 func (id NationalID) MarshalJSON() ([]byte, error) {
-	r := strings.NewReplacer("0", "*", "1", "*", "2", "*", "3", "*", "4", "*", "5", "*", "6", "*", "7", "*", "8", "*", "9", "*")
 	x := len(id) - 4
 
-	return json.Marshal(r.Replace(string(id)[:x]) + string(id)[x:])
+	return json.Marshal(nationalIDReplacer.Replace(string(id)[:x]) + string(id)[x:])
 }
 
 type EmailAddress string
